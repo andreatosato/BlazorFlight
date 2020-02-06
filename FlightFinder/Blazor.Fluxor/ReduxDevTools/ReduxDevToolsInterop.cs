29f961493e7bd0eca8b34e6e6c5dd9927eaa0890
Andrea Tosato <andrea.tosato@hotmail.it>
2020-02-06 06:52:36 +0100

    Create Project

---
+++ FlightFinder/Blazor.Fluxor/ReduxDevTools/ReduxDevToolsInterop.cs
@@ -0,0 +1,154 @@
+﻿using Blazor.Fluxor.ReduxDevTools.CallbackObjects;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Json = System.Text.Json.JsonSerializer;
+
+namespace Blazor.Fluxor.ReduxDevTools
+{
+	/// <summary>
+	/// Interop for dev tools
+	/// </summary>
+	public sealed class ReduxDevToolsInterop : IDisposable
+	{
+
+		internal const string DevToolsCallbackId = "DevToolsCallback";
+		internal bool DevToolsBrowserPluginDetected { get; private set; }
+		internal event EventHandler<JumpToStateCallback> JumpToState;
+		internal event EventHandler AfterJumpToState;
+		internal event EventHandler Commit;
+
+		private const string FluxorDevToolsId = "__FluxorDevTools__";
+		private const string FromJsDevToolsDetectedActionTypeName = "detected";
+		private const string ToJsDispatchMethodName = "dispatch";
+		private const string ToJsInitMethodName = "init";
+		private readonly IJSRuntime JSRuntime;
+		private bool IsInitializing;
+		private DotNetObjectReference<ReduxDevToolsInterop> DotNetRef;
+
+		/// <summary>
+		/// Creates an instance of the dev tools interop
+		/// </summary>
+		/// <param name="jsRuntime"></param>
+		public ReduxDevToolsInterop(IJSRuntime jsRuntime)
+		{
+			JSRuntime = jsRuntime;
+			DotNetRef = DotNetObjectReference.Create(this);
+		}
+
+		internal async ValueTask InitAsync(IDictionary<string, object> state)
+		{
+			IsInitializing = true;
+			try
+			{
+				await InvokeFluxorDevToolsMethodAsync<object>(ToJsInitMethodName, DotNetRef, state);
+			}
+			finally
+			{
+				IsInitializing = false;
+			}
+		}
+
+		internal void Dispatch(object action, IDictionary<string, object> state)
+		{
+			InvokeFluxorDevToolsMethodAsync<object>(ToJsDispatchMethodName, new ActionInfo(action), state);
+		}
+
+		/// <summary>
+		/// Called back from ReduxDevTools
+		/// </summary>
+		/// <param name="messageAsJson"></param>
+		[JSInvokable(DevToolsCallbackId)]
+		//TODO: Make private https://github.com/aspnet/Blazor/issues/1218
+		public void DevToolsCallback(string messageAsJson)
+		{
+			if (string.IsNullOrWhiteSpace(messageAsJson))
+				return;
+
+			var message = Json.Deserialize<BaseCallbackObject>(messageAsJson);
+			switch (message?.payload?.type)
+			{
+				case FromJsDevToolsDetectedActionTypeName:
+					DevToolsBrowserPluginDetected = true;
+					break;
+
+				case "COMMIT":
+					Commit?.Invoke(null, EventArgs.Empty);
+					break;
+
+				case "JUMP_TO_STATE":
+				case "JUMP_TO_ACTION":
+					OnJumpToState(Json.Deserialize<JumpToStateCallback>(messageAsJson));
+					break;
+			}
+		}
+
+#pragma warning disable CA1063 // Implement IDisposable Correctly
+		void IDisposable.Dispose()
+#pragma warning restore CA1063 // Implement IDisposable Correctly
+		{
+			DotNetRef.Dispose();
+		}
+
+		private ValueTask<TResult> InvokeFluxorDevToolsMethodAsync<TResult>(string identifier, params object[] args)
+		{
+			if (!DevToolsBrowserPluginDetected && !IsInitializing)
+				return new ValueTask<TResult>(default(TResult));
+
+			string fullIdentifier = $"{FluxorDevToolsId}.{identifier}";
+			return JSRuntime.InvokeAsync<TResult>(fullIdentifier, args);
+		}
+
+		private void OnJumpToState(JumpToStateCallback jumpToStateCallback)
+		{
+			JumpToState?.Invoke(null, jumpToStateCallback);
+			AfterJumpToState?.Invoke(null, EventArgs.Empty);
+		}
+
+		internal static string GetClientScripts()
+		{
+			string assemblyName = typeof(ReduxDevToolsInterop).Assembly.GetName().Name;
+
+			return $@"
+window.{FluxorDevToolsId} = new (function() {{
+	const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__;
+	this.{ToJsInitMethodName} = function() {{}};
+
+	if (reduxDevTools !== undefined && reduxDevTools !== null) {{
+		const fluxorDevTools = reduxDevTools.connect({{ name: 'Blazor-Fluxor' }});
+		if (fluxorDevTools !== undefined && fluxorDevTools !== null) {{
+			fluxorDevTools.subscribe((message) => {{ 
+				if (window.fluxorDevToolsDotNetInterop) {{
+					const messageAsJson = JSON.stringify(message);
+					window.fluxorDevToolsDotNetInterop.invokeMethodAsync('{DevToolsCallbackId}', messageAsJson); 
+				}}
+			}});
+		}}
+
+		this.{ToJsInitMethodName} = function(dotNetCallbacks, state) {{
+			window.fluxorDevToolsDotNetInterop = dotNetCallbacks;
+			fluxorDevTools.init(state);
+
+			if (window.fluxorDevToolsDotNetInterop) {{
+				// Notify Fluxor of the presence of the browser plugin
+				const detectedMessage = {{
+					payload: {{
+						type: '{ReduxDevToolsInterop.FromJsDevToolsDetectedActionTypeName}'
+					}}
+				}};
+				const detectedMessageAsJson = JSON.stringify(detectedMessage);
+				window.fluxorDevToolsDotNetInterop.invokeMethodAsync('{DevToolsCallbackId}', detectedMessageAsJson);
+			}}
+		}};
+
+		this.{ToJsDispatchMethodName} = function(action, state) {{
+			fluxorDevTools.send(action, state);
+		}};
+
+	}}
+}})();
+";
+		}
+	}
+}
---
+++ FlightFinder/Blazor.Fluxor/Store.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Blazor.Fluxor
+{
+	/// <see cref="IStore"/>
+	public class Store : IStore
+	{
+		/// <see cref="IStore.Features"/>
+		public IReadOnlyDictionary<string, IFeature> Features => FeaturesByName;
+		/// <see cref="IStore.Initialized"/>
+		public Task Initialized => InitializedCompletionSource.Task;
+
+		private readonly object SyncRoot = new object();
+		private IStoreInitializationStrategy StoreInitializationStrategy;
+		private readonly Dictionary<string, IFeature> FeaturesByName = new Dictionary<string, IFeature>(StringComparer.InvariantCultureIgnoreCase);
+		private readonly List<IEffect> Effects = new List<IEffect>();
+		private readonly List<IMiddleware> Middlewares = new List<IMiddleware>();
+		private readonly List<IMiddleware> ReversedMiddlewares = new List<IMiddleware>();
+		private readonly Queue<object> QueuedActions = new Queue<object>();
+		private readonly TaskCompletionSource<bool> InitializedCompletionSource = new TaskCompletionSource<bool>();
+
+		private volatile bool IsDispatching;
+		private volatile int BeginMiddlewareChangeCount;
+		private volatile bool HasActivatedStore;
+		private bool IsInsideMiddlewareChange => BeginMiddlewareChangeCount > 0;
+		private Action<IFeature, object> IFeatureReceiveDispatchNotificationFromStore;
+
+		/// <summary>
+		/// Creates an instance of the store
+		/// </summary>
+		/// <param name="storeInitializationStrategy">The strategy used to initialise the store</param>
+		public Store(IStoreInitializationStrategy storeInitializationStrategy)
+		{
+			StoreInitializationStrategy = storeInitializationStrategy;
+
+			MethodInfo dispatchNotifictionFromStoreMethodInfo =
+				typeof(IFeature)
+				.GetMethod(nameof(IFeature.ReceiveDispatchNotificationFromStore));
+			IFeatureReceiveDispatchNotificationFromStore = (Action<IFeature, object>)
+				Delegate.CreateDelegate(typeof(Action<IFeature, object>), dispatchNotifictionFromStoreMethodInfo);
+
+			Dispatch(new StoreInitializedAction());
+		}
+
+		/// <see cref="IStore.AddFeature(IFeature)"/>
+		public void AddFeature(IFeature feature)
+		{
+			if (feature == null)
+				throw new ArgumentNullException(nameof(feature));
+
+			lock (SyncRoot)
+			{
+				FeaturesByName.Add(feature.GetName(), feature);
+			}
+		}
+
+		/// <see cref="IDispatcher.Dispatch(object)"/>
+		public void Dispatch(object action)
+		{
+			if (action == null)
+				throw new ArgumentNullException(nameof(action));
+
+			lock (SyncRoot)
+			{
+				// Do not allow task dispatching inside a middleware-change.
+				// These change cycles are for things like "jump to state" in Redux Dev Tools
+				// and should be short lived.
+				// We avoid dispatching inside a middleware change because we don't want UI events (like component Init)
+				// that trigger actions (such as fetching data from a server) to execute
+				if (IsInsideMiddlewareChange)
+					return;
+
+				// If a dequeue is already in progress, we will just
+				// let this new action be added to the queue and then exit
+				// Note: This is to cater for the following scenario
+				//	1: An action is dispatched
+				//	2: An effect is triggered
+				//	3: The effect immediately dispatches a new action
+				// The Queue ensures it is processed after its triggering action has completed rather than immediately
+				QueuedActions.Enqueue(action);
+
+				// HasActivatedStore is set to true when the page finishes loading
+				// At which point DequeueActions will be called
+				if (!HasActivatedStore)
+					return;
+
+				DequeueActions();
+			}
+		}
+
+		/// <see cref="IStore.AddEffect(IEffect)"/>
+		public void AddEffect(IEffect effect)
+		{
+			if (effect == null)
+				throw new ArgumentNullException(nameof(effect));
+
+			lock (SyncRoot)
+			{
+				Effects.Add(effect);
+			}
+		}
+
+		/// <see cref="IStore.AddMiddleware(IMiddleware)"/>
+		public void AddMiddleware(IMiddleware middleware)
+		{
+			lock (SyncRoot)
+			{
+				Middlewares.Add(middleware);
+				ReversedMiddlewares.Insert(0, middleware);
+				// Initialize the middleware immediately if the store has already been initialized, otherwise this will be
+				// done the first time Dispatch is called
+				if (HasActivatedStore)
+				{
+					middleware.InitializeAsync(this).Wait();
+					middleware.AfterInitializeAllMiddlewares();
+				}
+			}
+		}
+
+		/// <see cref="IStore.BeginInternalMiddlewareChange"/>
+		public IDisposable BeginInternalMiddlewareChange()
+		{
+			lock (SyncRoot)
+			{
+				BeginMiddlewareChangeCount++;
+				IDisposable[] disposables = Middlewares
+					.Select(x => x.BeginInternalMiddlewareChange())
+					.ToArray();
+
+				return new DisposableCallback(() => EndMiddlewareChange(disposables));
+			}
+		}
+
+		/// <see cref="IStore.Initialize"/>
+		public RenderFragment Initialize()
+		{
+			if (HasActivatedStore)
+				return builder => { };
+
+			StoreInitializationStrategy.Initialize(ActivateStore);
+			return (RenderTreeBuilder renderer) =>
+			{
+				var scriptBuilder = new StringBuilder();
+				scriptBuilder.AppendLine("if (window.canInitializeFluxor) {");
+				{
+					scriptBuilder.AppendLine("delete window.canInitializeFluxor;");
+					foreach (IMiddleware middleware in Middlewares)
+					{
+						string middlewareScript = middleware.GetClientScripts();
+						if (middlewareScript != null)
+						{
+							scriptBuilder.AppendLine($"// Middleware scripts: {middleware.GetType().FullName}");
+							scriptBuilder.AppendLine($"{middlewareScript}");
+						}
+					}
+				}
+				scriptBuilder.AppendLine("}");
+
+				string script = scriptBuilder.ToString();
+				renderer.OpenElement(1, "script");
+				renderer.AddAttribute(2, "id", "initializeFluxor");
+				renderer.AddMarkupContent(3, script);
+				renderer.CloseElement();
+			};
+		}
+
+		private void EndMiddlewareChange(IDisposable[] disposables)
+		{
+			lock (SyncRoot)
+			{
+				BeginMiddlewareChangeCount--;
+				if (BeginMiddlewareChangeCount == 0)
+					disposables.ToList().ForEach(x => x.Dispose());
+			}
+		}
+
+		private void TriggerEffects(object action)
+		{
+			var effectsToTrigger = Effects.Where(x => x.ShouldReactToAction(action));
+			foreach (var effect in effectsToTrigger)
+				effect.HandleAsync(action, this);
+		}
+
+		private async void InitializeMiddlewares()
+		{
+			foreach (IMiddleware middleware in Middlewares)
+			{
+				await middleware.InitializeAsync(this);
+			}
+			Middlewares.ForEach(x => x.AfterInitializeAllMiddlewares());
+		}
+
+		private void ExecuteMiddlewareBeforeDispatch(object actionAboutToBeDispatched)
+		{
+			foreach (IMiddleware middleWare in Middlewares)
+				middleWare.BeforeDispatch(actionAboutToBeDispatched);
+		}
+
+		private void ExecuteMiddlewareAfterDispatch(object actionJustDispatched)
+		{
+			Middlewares.ForEach(x => x.AfterDispatch(actionJustDispatched));
+		}
+
+		private void ActivateStore()
+		{
+			if (HasActivatedStore)
+				return;
+
+			lock (SyncRoot)
+			{
+				HasActivatedStore = true;
+				InitializeMiddlewares();
+				DequeueActions();
+				InitializedCompletionSource.SetResult(true);
+			}
+		}
+
+		private void DequeueActions()
+		{
+			if (IsDispatching)
+				return;
+
+			IsDispatching = true;
+			try
+			{
+				while (QueuedActions.TryDequeue(out object nextActionToProcess))
+				{
+					// Only process the action if no middleware vetos it
+					if (Middlewares.All(x => x.MayDispatchAction(nextActionToProcess)))
+					{
+						ExecuteMiddlewareBeforeDispatch(nextActionToProcess);
+
+						// Notify all features of this action
+						foreach (var featureInstance in FeaturesByName.Values)
+							IFeatureReceiveDispatchNotificationFromStore(featureInstance, nextActionToProcess);
+
+						ExecuteMiddlewareAfterDispatch(nextActionToProcess);
+
+						TriggerEffects(nextActionToProcess);
+					}
+				}
+			}
+			finally
+			{
+				IsDispatching = false;
+			}
+		}
+	}
+}
